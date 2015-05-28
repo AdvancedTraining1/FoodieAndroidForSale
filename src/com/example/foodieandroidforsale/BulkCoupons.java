241ec8a4bf624f10fac4364bbdee888266e4e3fb
tiancaifox <14126099@bjtu.edu.cn>
2015-05-28 21:09:31 +0800

    给所有用户端推送消息完成啦

--- src/com/example/foodieandroidforsale/BulkCoupons.java
+++ src/com/example/foodieandroidforsale/BulkCoupons.java
@@ -1,12 +1,33 @@
 package com.example.foodieandroidforsale;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONException;
+import org.json.JSONObject;
 import org.json.JSONStringer;
 
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -28,6 +49,62 @@ public class BulkCoupons extends Activity {
         
         buttonInputOK.setOnClickListener(new OnClickListener() {      
             public void onClick(View v) {
+            	
+            	if (getTextTitle()==null || "".equals(getTextTitle())
+            			|| getTextContent()==null || "".equals(getTextContent())) {
+            		Toast.makeText(BulkCoupons.this, "题目和内容不能为空，请重新填写",Toast.LENGTH_LONG ).show(); 
+            		return;
+        		}
+            			
+    			StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()       
+                .detectDiskReads()       
+                .detectDiskWrites()       
+                .detectNetwork()   // or .detectAll() for all detectable problems       
+                .penaltyLog()       
+                .build()); 
+            			
+    			 String serviceAddr = "http://101.200.174.49:3000/service/pushmsg"; 
+                 List <NameValuePair> params=new ArrayList<NameValuePair>();  
+                 params.add(new BasicNameValuePair("msg",getTextTitle()));  
+                 params.add(new BasicNameValuePair("head",getTextContent()));
+                 
+                 String result = null;
+     			 BufferedReader reader = null;
+     			try {
+     				DefaultHttpClient client = new DefaultHttpClient();
+     				HttpPost request = new HttpPost();
+     				request.setURI(new URI(serviceAddr));
+     				
+
+     				UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(params,HTTP.UTF_8);
+     				request.setEntity(formEntity);
+
+     				System.out.println("url-------"+request.toString());
+     				HttpResponse response = client.execute(request);
+     				reader = new BufferedReader(new InputStreamReader(response
+     						.getEntity().getContent()));
+
+     				StringBuffer strBuffer = new StringBuffer("");
+     				String line = null;
+     				while ((line = reader.readLine()) != null) {
+     					strBuffer.append(line);
+     				}
+     				result = strBuffer.toString();
+
+     			} catch (Exception e) {
+     				e.printStackTrace();
+     			} finally {
+     				if (reader != null) {
+     					try {
+     						reader.close();
+     						reader = null;
+     					} catch (IOException e) {
+     						e.printStackTrace();
+     					}
+     				}
+     			}
+                System.out.println(result);
+     
                 
                 SharedPreferences preferences = getSharedPreferences("Text", 0);
                 SharedPreferences.Editor editor = preferences.edit();
@@ -39,7 +116,8 @@ public class BulkCoupons extends Activity {
                    
                 }
                 finish();
-             }
+               }
+ 
         });
         
         
